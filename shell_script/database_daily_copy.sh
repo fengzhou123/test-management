425cfef1ab1e4e03d2b2d284b49a9a9793fdff43
Feng Zhou <101240484+fengzhou123@users.noreply.github.com>
2022-03-10 15:27:44 +1300

    Updated database_daily_copy.sh with the message "It triggers a Jenkins project"

--- shell_script/database_daily_copy.sh
+++ shell_script/database_daily_copy.sh
@@ -5,3 +5,5 @@ currDate=`date '+%Y%m%d'`
 /src/kiwi_9.40.1_01oct2020/bin/dbcopy nzvader:kdw_9741_master_datawarehouse nzvader:9741bk_${currDate}_master_datawarehouse
 
 /src/kiwi_9.40.1_01oct2020/bin/dbcopy nzvader:kdw_9741_working_datawarehouse nzvader:9741bk_${currDate}_working_datawarehouse
+
+echo "It triggers a Jenkins project"
