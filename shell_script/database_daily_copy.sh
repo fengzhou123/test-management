d4861a0326d1c4915b3ed179615c246bd68ef858
Feng Zhou <101240484+fengzhou123@users.noreply.github.com>
2022-03-10 15:22:35 +1300

    Create database_daily_copy.sh

---
+++ shell_script/database_daily_copy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+currDate=`date '+%Y%m%d'`
+
+/src/kiwi_9.40.1_01oct2020/bin/dbcopy nzvader:kdw_9741_master_datawarehouse nzvader:9741bk_${currDate}_master_datawarehouse
+
+/src/kiwi_9.40.1_01oct2020/bin/dbcopy nzvader:kdw_9741_working_datawarehouse nzvader:9741bk_${currDate}_working_datawarehouse
