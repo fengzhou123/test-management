e999b3f88de09251edcdaaadf710f5db746a722c
Feng Zhou <101240484+fengzhou123@users.noreply.github.com>
2022-03-14 14:42:54 +1300

    Update database_daily_copy.sh

--- shell_script/database_daily_copy.sh
+++ shell_script/database_daily_copy.sh
@@ -6,4 +6,3 @@ currDate=`date '+%Y%m%d'`
 
 /src/kiwi_9.40.1_01oct2020/bin/dbcopy nzvader:kdw_9741_working_datawarehouse nzvader:9741bk_${currDate}_working_datawarehouse
 
-echo "It triggers a Jenkins project"
